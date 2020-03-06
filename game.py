b1e0c250a9f88a7439445b4aeff7863d45be07c8
danielbosnich <danielbosnich@gmail.com>
2020-03-05 17:28:01 -0700

    Update game.py
    Updated logging to use f-strings

--- game.py
+++ game.py
@@ -99,7 +99,7 @@ class Game():
 
     def _display_possible_moves(self, event, piece):
         """Displays all possible moves for the piece in a given position"""
-        logging.debug('Button click event was %s', event)
+        logging.debug(f'Button click event was {event}')
         # Don't show moves if it's not their turn
         if piece.color != self._turn_color:
             self._clear_possible_moves()
@@ -107,8 +107,8 @@ class Game():
 
         # Show potential moves for a given square
         if piece.position != self._previous_position_shown:
-            logging.info('Showing possible moves for the %s %s at %s',
-                         piece.color, piece.piece_type, piece.position)
+            logging.info(f'Showing possible moves for the {piece.color} '
+                         f'{piece.piece_type} at {piece.position}')
             if self._possible_move_buttons:
                 self._clear_possible_moves()
             # Check potential moves for the piece
@@ -123,8 +123,8 @@ class Game():
 
         # Stop showing the potential moves for a given square
         else:
-            logging.info("Clearing moves")
-            logging.debug("Removing the potential moves from the board")
+            logging.info('Clearing moves')
+            logging.debug('Removing the potential moves from the board')
             self._clear_possible_moves()
             self._previous_position_shown = None
 
@@ -148,8 +148,8 @@ class Game():
 
     def _remove_piece(self, piece):
         """Removes the passed piece"""
-        logging.info('Removing the %s %s at %s',
-                     piece.color, piece.piece_type, piece.position)
+        logging.info(f'Removing the {piece.color} {piece.piece_type} at '
+                     f'{piece.position}')
         piece.button.destroy()
         self._pieces.remove(piece)
         del piece
@@ -184,44 +184,39 @@ class Game():
     def _move_piece(self, event, piece, new_position):
         """Moves the chess piece to a new position and checks some conditions
         after the move"""
-        logging.debug('Button click event was %s', event)
+        logging.debug(f'Button click event was {event}')
         self._clear_possible_moves()
         piece.check_potential_moves(self._return_squares())
         # Check if this is a capture
         captured_piece = None
         if new_position in piece.possible_captures:
             captured_piece = self.return_piece(new_position)
-            logging.info("The %s %s at position %s will be captured!",
-                         captured_piece.color,
-                         captured_piece.piece_type,
-                         new_position)
+            logging.info(f'The {captured_piece.color} {captured_piece.piece_type}'
+                         f'at position {new_position} will be captured!')
             # Remove the piece being captured
             self._remove_piece(captured_piece)
 
         # Change the moving piece's location
-        logging.info("Moving the %s %s from %s to %s",
-                     piece.color,
-                     piece.piece_type,
-                     piece.position,
-                     new_position)
+        logging.info(f'Moving the {piece.color} {piece.piece_type} from '
+                     f'{piece.position} to {new_position}')
         piece.update_position(new_position)
 
         # Check if the king was captured and, if so, end the game
         if captured_piece is not None and captured_piece.piece_type == KING:
-            logging.info("The %s king has just been captured. The game is over.",
-                         captured_piece.color)
+            logging.info(f'The {captured_piece.color} king has just been '
+                         f'captured. The game is over.')
             self._show_game_over(piece.color)
             return
 
         # Check if the previous move deserves a promotion
         if piece.piece_type == PAWN:
             if piece.color == WHITE and new_position[0] == "8":
-                logging.info("The %s %s at position %s is up for promotion",
-                             WHITE, PAWN, new_position)
+                logging.info(f'The {WHITE} {PAWN} at position {new_position} '
+                             f'is up for promotion')
                 self.promotion(piece)
             elif piece.color == BLACK and new_position[0] == "1":
-                logging.info("The %s %s at position %s is up for promotion",
-                             BLACK, PAWN, new_position)
+                logging.info(f'The {BLACK} {PAWN} at position {new_position} '
+                             f'is up for promotion')
                 self.promotion(piece)
 
         # Check if the previous move was an En Passent or Castle
@@ -286,19 +281,19 @@ class Game():
         """Completes pawn promotion by creating a promotion """
         self._promotion_display = PromotionDisplay()
         # Wait for the user input
-        logging.info("Waiting")
+        logging.info('Waiting')
         self._display.root.wait_window(self._promotion_display.root)
-        logging.info("Finished waiting")
+        logging.info('Finished waiting')
         # If the user closed the window without choosing a piece then just
         # leave the pawn
         if self._promotion_display.chosen_piece is None:
-            logging.info("No piece was chosen, leaving the pawn")
+            logging.info('No piece was chosen, leaving the pawn')
             return
         # Otherwise, change the piece out with the selected type
         chosen_piece = self._promotion_display.chosen_piece
         logging.info(chosen_piece)
         if chosen_piece == QUEEN:
-            logging.info("HERE")
+            logging.info('HERE')
             new_piece = Queen(piece.color, piece.position, self._display.root)
         elif chosen_piece == KNIGHT:
             new_piece = Knight(piece.color, piece.position, self._display.root)
@@ -329,9 +324,7 @@ class Game():
                 piece.check_potential_moves(squares)
                 for move in piece.possible_captures:
                     if squares[move].piece_type == KING:
-                        logging.info("The %s %s at position %s is in check!",
-                                     squares[move].color,
-                                     KING,
-                                     move)
+                        logging.info(f'The {squares[move].color} {KING} at '
+                                     f'position {move} is in check!')
                         return True
         return False
